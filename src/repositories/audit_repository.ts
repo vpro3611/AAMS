1032d2cc9ff2113c4479b3636cdcb0e7bc925d10
vpro3611 <vprov8645@gmail.com>
2026-01-26 19:48:30 +0100

    Repositories for users and audit events

---
+++ src/repositories/audit_repository.ts
@@ -0,0 +1,34 @@
+import { pool } from '../database';
+import { Pool, PoolClient } from "pg";
+import { AuditEvent, NewAuditEvent } from "../models/models";
+
+
+export class AuditRepository {
+    constructor(private readonly client: Pool | PoolClient = pool) {}
+
+    private mapAuditEvent = (row: any): AuditEvent => ({
+        id: row.id,
+        actor_user_id: row.actor_user_id,
+        action: row.action,
+        created_at: row.created_at,
+    });
+
+    createAuditEvent = async (newAuditEvent: NewAuditEvent): Promise<AuditEvent> => {
+        const { actor_user_id, action } = newAuditEvent;
+
+        const res = await this.client.query('INSERT INTO audit_events (actor_user_id, action) VALUES ($1, $2)' +
+            ' RETURNING *',
+            [actor_user_id, action]);
+
+        return this.mapAuditEvent(res.rows[0]);
+    }
+
+    getAuditEvents = async (): Promise<AuditEvent[]> => {
+        const res = await this.client.query('SELECT * FROM audit_events');
+
+        if (res.rowCount === 0) return [];
+
+        return res.rows.map(this.mapAuditEvent);
+    }
+
+}
---
+++ src/repositories/user_repository.ts
@@ -0,0 +1,34 @@
+import { pool } from '../database';
+import {NewUser, User} from "../models/models";
+import {Pool, PoolClient} from "pg";
+
+export class UserRepository {
+
+    constructor(private readonly client: Pool | PoolClient = pool) {}
+
+    private mapUser = (row: any): User => ({
+        id: row.id,
+        email: row.email,
+        password_hash: row.password_hash,
+        status: row.status,
+        created_at: row.created_at,
+    });
+
+    findUserById = async (id: string): Promise<User | null> => {
+        const res = await this.client.query('SELECT * FROM users WHERE id = $1', [id]);
+        if (res.rowCount === 0) return null;
+        return this.mapUser(res.rows[0]);
+    }
+
+    findUserByEmail = async (email: string): Promise<User | null> => {
+        const res = await this.client.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (res.rowCount === 0) return null;
+        return this.mapUser(res.rows[0]);
+    }
+
+     createUser = async (newUser: NewUser): Promise<User> => {
+         const res = await this.client.query('INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING *',
+             [newUser.email, newUser.password_hash])
+         return this.mapUser(res.rows[0])
+     }
+}
