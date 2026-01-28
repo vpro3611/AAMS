2cd85d007720c3a1930fa4356b1835ef30c83cda
vpro3611 <vprov8645@gmail.com>
2026-01-28 21:09:14 +0100

    Add: Added a role service, validating the data and calling repositories methods

---
+++ src/services/role_service.ts
@@ -0,0 +1,32 @@
+import {RoleRepository} from "../repositories/role_repository";
+import {NewRole, Role} from "../models/models";
+
+
+export class RoleService {
+    constructor(private readonly roleRepo: RoleRepository) {}
+
+    createNewRole = async (newRole: NewRole): Promise<Role | null> => {
+        if (!newRole) throw new Error("New role cannot be null or undefined");
+        return this.roleRepo.createRole(newRole);
+    }
+
+    findRoleByName = async (name: string): Promise<Role | null> => {
+        if (!name) throw new Error("Role name cannot be null or undefined");
+        return this.roleRepo.findRoleByName(name);
+    }
+
+    getAllRoles = async (): Promise<Role[]> => {
+        return this.roleRepo.getRoles();
+    }
+
+    updateRole = async (roleId: string, newRole: NewRole): Promise<Role | null> => {
+        if (!roleId) throw new Error("Role id cannot be null or undefined");
+        if (!newRole) throw new Error("New role cannot be null or undefined");
+        return this.roleRepo.updateRole(roleId, newRole);
+    }
+
+    deleteRole = async (roleId: string): Promise<Role | null> => {
+        if (!roleId) throw new Error("Role id cannot be null or undefined");
+        return this.roleRepo.deleteRole(roleId);
+    }
+}
