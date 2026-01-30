44164bf61512ebb94bd5ba5aa6dcb18cf1387be8
vpro3611 <vprov8645@gmail.com>
2026-01-31 00:46:11 +0100

    Refactor: refactored errors, so now they are classes -> good for http

--- src/services/user_role_service.ts
+++ src/services/user_role_service.ts
@@ -1,26 +1,27 @@
 import {UserRoleRepository} from "../repositories/user_role_repository";
 import {UserRole, UserRoleWithNames} from "../models/models";
+import {BadRequestError, UserRoleNotFoundError, UserRolePersistenceError} from "../errors/errors";
 
 
 export class UserRoleService {
     constructor(private readonly userRoleRepo: UserRoleRepository ) {}
 
     assignRoleToUser = async (userId: string, roleId: string): Promise<UserRole> => {
-        if (!userId) throw new Error("Invalid user id");
-        if (!roleId) throw new Error("Invalid role id");
+        if (!userId) throw new BadRequestError("Invalid user id");
+        if (!roleId) throw new BadRequestError("Invalid role id");
         const res = await this.userRoleRepo.assignRoleToUser(userId, roleId);
-        if (!res) throw new Error("User role not found");
+        if (!res) throw new UserRolePersistenceError();
         return res;
     }
     getUserRoles = async (userId: string): Promise<UserRoleWithNames[]> => {
-        if (!userId) throw new Error("Invalid user id");
+        if (!userId) throw new BadRequestError("Invalid user id");
         return await this.userRoleRepo.getUserRoles(userId);
     }
     deleteUserRole = async (userId: string, roleId: string): Promise<UserRole> => {
-        if (!userId) throw new Error("Invalid user id");
-        if (!roleId) throw new Error("Invalid role id");
+        if (!userId) throw new BadRequestError("Invalid user id");
+        if (!roleId) throw new BadRequestError("Invalid role id");
         const res = await this.userRoleRepo.deleteUserRole(userId, roleId);
-        if (!res) throw new Error("User role not found");
+        if (!res) throw new UserRoleNotFoundError();
         return res;
     }
 }
