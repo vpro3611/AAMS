df5281bc62624603f032c86ebba49adc284b97b8
vpro3611 <vprov8645@gmail.com>
2026-02-01 00:58:22 +0100

    Add: added authentification middleware, dealing with JWT token

---
+++ src/middlewares/auth_middleware.ts
@@ -0,0 +1,28 @@
+import {TokenService} from "../models/models";
+import {NextFunction, Request, Response} from "express";
+
+declare global {
+    namespace Express {
+        interface Request {
+            userID?: string;
+        }
+    }
+}
+
+
+export const authMiddleware = (tokenService: TokenService) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) return res.sendStatus(401).json({message: "Unauthorized"});
+        const [type, token] = authHeader.split(" ");
+        if (type !== "Bearer") return res.sendStatus(401).json({message: "Unauthorized"});
+
+        try {
+            const payload = tokenService.verifyToken(token);
+            req.userID = payload.sub;
+            next();
+        } catch (error) {
+            return res.sendStatus(401).json({message: "Invalid Token"});
+        }
+    };
+};
