c4bdc793999c17483f3033746a109a7eb39a0dca
vpro3611 <vprov8645@gmail.com>
2026-02-01 19:13:10 +0100

    Refactor: refactored login accordingly to newly added error middleware

--- src/authentification/login_controller.ts
+++ src/authentification/login_controller.ts
@@ -1,13 +1,17 @@
 import {AuthService} from "../services/auth_service";
 import {UserService} from "../services/user_service";
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 
 export class LoginController {
     constructor(private readonly authServ: AuthService) {}
 
-    login = async(req: Request, res: Response)=> {
-        const { email, password } = req.body;
-        const result = await this.authServ.login({email, password});
-        res.json(result);
+    login = async(req: Request, res: Response, next: NextFunction)=> {
+        try {
+            const {email, password} = req.body;
+            const result = await this.authServ.login({email, password});
+            res.json(result);
+        } catch (e) {
+            next(e);
+        }
     }
 }
