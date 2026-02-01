87b8fbcef4fd235b58c90498e637ae8dbd1bb4ec
vpro3611 <vprov8645@gmail.com>
2026-02-01 19:13:38 +0100

    Refactor: refactored registration accordingly to newly added error middleware

--- src/authentification/registration_controller.ts
+++ src/authentification/registration_controller.ts
@@ -1,18 +1,20 @@
 import {UserService} from "../services/user_service";
 import {AuthService} from "../services/auth_service";
-import {Request, Response} from "express";
+import {Request, Response, NextFunction} from "express";
 
 export class RegisterController {
     constructor(private readonly authServ: AuthService) {}
 
-    registerUser = async(req: Request, res: Response) => {
-        const { email, password } = req.body;
-
-        const user = await this.authServ.register({
-            email,
-            password,
-        });
-
-        res.status(201).json(user);
+    registerUser = async(req: Request, res: Response, next: NextFunction) => {
+        try {
+            const {email, password} = req.body;
+            const user = await this.authServ.register({
+                email,
+                password,
+            });
+            res.status(201).json(user);
+        } catch (e) {
+            next(e);
+        }
     }
 }
