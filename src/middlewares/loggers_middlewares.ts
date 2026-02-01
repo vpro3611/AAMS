523d956c758f9253060daf75309cbd0db9cfb1df
vpro3611 <vprov8645@gmail.com>
2026-02-01 19:10:29 +0100

    Add: added request and response loggers

---
+++ src/middlewares/loggers_middlewares.ts
@@ -0,0 +1,25 @@
+import {NextFunction, Request, Response} from "express";
+
+declare global {
+    namespace Express {
+        interface Request {
+            userID?: string;
+        }
+    }
+}
+
+export const loggerMiddleware = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        console.log(`Request: [${new Date().toISOString()}] ${req.method} ${req.originalUrl} by ${req.userID || "anonymous"}`);
+        next();
+    };
+}
+
+export const ResponseLoggerMiddleware = () => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        res.on("finish", () => {
+            console.log(`Response: [${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+        });
+        next();
+    };
+}
