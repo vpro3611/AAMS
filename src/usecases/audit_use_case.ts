e12fc95aaf21435b610d005946b7d21679892619
vpro3611 <vprov8645@gmail.com>
2026-02-02 23:45:28 +0100

    Add: added new use case methods - get by userId and by action

--- src/usecases/audit_use_case.ts
+++ src/usecases/audit_use_case.ts
@@ -31,4 +31,52 @@ export class AuditUseCase {
             client.release();
         }
     }
+
+    getAuditEventsByUserId = async (actorId: string, userId: string): Promise<AuditEvent[]> => {
+        if (!actorId) throw new BadRequestError("Actor ID is required");
+
+        const client = await this.pool.connect();
+
+        try {
+            await client.query("BEGIN")
+
+            const auditRepo = new AuditRepository(client);
+            const auditServ = new AuditService(auditRepo);
+
+            const allAuditEvents = await auditServ.getAuditEventsByUserId(userId);
+            await auditServ.log(actorId, AuditAction.GET_AUDIT_EVENTS_BY_USER_ID);
+
+            await client.query("COMMIT");
+            return allAuditEvents;
+        } catch (e) {
+            await client.query("ROLLBACK");
+            throw e;
+        } finally {
+            client.release();
+        }
+    }
+
+    getAuditEventsByAction = async (actorId: string, action: AuditAction): Promise<AuditEvent[]> => {
+        if (!actorId) throw new BadRequestError("Actor ID is required");
+
+        const client = await this.pool.connect();
+
+        try {
+            await client.query("BEGIN")
+
+            const auditRepo = new AuditRepository(client);
+            const auditServ = new AuditService(auditRepo);
+
+            const allAuditEvents = await auditServ.getAuditEventsByAction(action);
+            await auditServ.log(actorId, AuditAction.GET_AUDIT_EVENTS_BY_ACTION);
+
+            await client.query("COMMIT");
+            return allAuditEvents;
+        } catch (e) {
+            await client.query("ROLLBACK");
+            throw e;
+        } finally {
+            client.release();
+        }
+    }
 }
