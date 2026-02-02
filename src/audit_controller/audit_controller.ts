0b93624dade516d10a58e8f4f272d1a52f8c1c9f
vpro3611 <vprov8645@gmail.com>
2026-02-03 00:13:08 +0100

    Add: added new methods to audit_controller

--- src/audit_controller/audit_controller.ts
+++ src/audit_controller/audit_controller.ts
@@ -1,4 +1,3 @@
-import {AuditService} from "../services/audit_service";
 import {AuditUseCase} from "../usecases/audit_use_case";
 import {NextFunction, Request, Response} from "express";
 
@@ -10,6 +9,28 @@ export class AuditController {
         try {
             const actorId = req.userID;
             const auditLogs = await this.auditUseCase.getAuditEvents(actorId!);
+            res.status(200).json(auditLogs);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    getAuditLogsByUserId = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const actorId = req.userID;
+            const {userId} = req.body;
+            const auditLogs = await this.auditUseCase.getAuditEventsByUserId(actorId!, userId);
+            res.status(200).json(auditLogs);
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    getAuditLogsByAction = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const actorId = req.userID;
+            const {action} = req.body;
+            const auditLogs = await this.auditUseCase.getAuditEventsByAction(actorId!, action);
             res.status(200).json(auditLogs);
         } catch (error) {
             next(error);
