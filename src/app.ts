7b10d3964110a955dc101de4f7462b621c0d41c8
vpro3611 <vprov8645@gmail.com>
2026-02-03 21:27:36 +0100

    Add: added usage of role middleware to private routes, distributing them among admins and moderators

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { authMiddleware } from "./middlewares/auth_middleware";
 import { userStatusCheckMiddleware } from "./middlewares/user_status_check_middleware";
 import { setUserStatus } from "./middlewares/user_status_check_middleware";
 import { errorsMiddleware } from "./middlewares/errors_middleware";
-import {TokenService} from "./models/models";
+import {Roles, TokenService} from "./models/models";
 import {UserService} from "./services/user_service";
 import {RegisterController} from "./authentification_controllers/registration_controller";
 import {LoginController} from "./authentification_controllers/login_controller";
@@ -16,6 +16,8 @@ import {UserController} from "./controllers/user_controllers";
 import {RoleController} from "./controllers/role_controllers";
 import {AuditController} from "./controllers/audit_controller";
 import {UserRoleController} from "./controllers/user_role_controller";
+import {requireRole, requireAnyRole, attachUserRoles} from "./middlewares/roles_middleware";
+import {UserRoleService} from "./services/user_role_service";
 
 export function createApp(deps: {
     tokenService: TokenService;
@@ -26,6 +28,7 @@ export function createApp(deps: {
     roleController: RoleController;
     auditController: AuditController;
     userRoleController: UserRoleController;
+    userRoleServ: UserRoleService;
 }) {
     const app = express();
 
@@ -44,34 +47,38 @@ export function createApp(deps: {
     publicRouter.post("/login", deps.loginController.login);
     app.use("/", publicRouter);
 
+    const adminOnly = requireRole(Roles.ADMIN);
+    const adminOrModerator = requireAnyRole(Roles.ADMIN, Roles.MODERATOR);
+
     // private
     const privateRouter = express.Router();
 
     privateRouter.use(authMiddleware(deps.tokenService));
+    privateRouter.use(attachUserRoles(deps.userRoleServ));
     privateRouter.use(setUserStatus(deps.userServ));
     privateRouter.use(userStatusCheckMiddleware());
     privateRouter.use(loggerMiddleware());
 
-    privateRouter.patch("/block_user", deps.userController.blockUser);
-    privateRouter.patch("/unblock_user", deps.userController.unblockUser);
-    privateRouter.post("/user/get_id", deps.userController.findUserById);
-    privateRouter.post("/user/get_email", deps.userController.findUserByEmail);
-    privateRouter.get("/users", deps.userController.getAllUsers);
-    privateRouter.post("/user/delete", deps.userController.deleteUser);
+    privateRouter.patch("/block_user", adminOnly, deps.userController.blockUser);
+    privateRouter.patch("/unblock_user", adminOnly, deps.userController.unblockUser);
+    privateRouter.post("/user/get_id", adminOrModerator, deps.userController.findUserById);
+    privateRouter.post("/user/get_email", adminOrModerator, deps.userController.findUserByEmail);
+    privateRouter.get("/users", adminOrModerator, deps.userController.getAllUsers);
+    privateRouter.post("/user/delete", adminOnly, deps.userController.deleteUser);
 
-    privateRouter.post("/role", deps.roleController.createRole);
-    privateRouter.post("/role/get_name", deps.roleController.findRoleByName)
-    privateRouter.get("/roles", deps.roleController.getAllRoles);
-    privateRouter.patch("/role/update", deps.roleController.updateRole);
-    privateRouter.post("/role/delete", deps.roleController.deleteRole);
+    privateRouter.post("/role", adminOrModerator, deps.roleController.createRole);
+    privateRouter.post("/role/get_name", adminOrModerator, deps.roleController.findRoleByName)
+    privateRouter.get("/roles", adminOrModerator, deps.roleController.getAllRoles);
+    privateRouter.patch("/role/update", adminOrModerator,deps.roleController.updateRole);
+    privateRouter.post("/role/delete", adminOrModerator, deps.roleController.deleteRole);
 
-    privateRouter.get("/audit_logs", deps.auditController.getAuditLogs);
-    privateRouter.post("/audit_logs/get_user", deps.auditController.getAuditLogsByUserId);
-    privateRouter.post("/audit_logs/get_action", deps.auditController.getAuditLogsByAction);
+    privateRouter.get("/audit_logs", adminOrModerator, deps.auditController.getAuditLogs);
+    privateRouter.post("/audit_logs/get_user", adminOrModerator,deps.auditController.getAuditLogsByUserId);
+    privateRouter.post("/audit_logs/get_action", adminOrModerator,deps.auditController.getAuditLogsByAction);
 
-    privateRouter.post("/assign_role", deps.userRoleController.assignRoleToUser);
-    privateRouter.post("/get_roles", deps.userRoleController.getUserRoles);
-    privateRouter.post("/remove_role", deps.userRoleController.removeRoleFromUser);
+    privateRouter.post("/assign_role", adminOnly,deps.userRoleController.assignRoleToUser);
+    privateRouter.post("/get_roles", adminOnly,deps.userRoleController.getUserRoles);
+    privateRouter.post("/remove_role", adminOnly,deps.userRoleController.removeRoleFromUser);
     app.use("/api", privateRouter);
 
     // errors LAST
